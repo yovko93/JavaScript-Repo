6adb2e32ec99ba492ba5faa6edc39300dfb8c3d4
Yovko Garkov <iovko1993@gmail.com>
2021-01-27 00:11:56 -0800

    Add DOM Introduction

--- JS Advanced/DOM Introduction/1. Subtraction/subtract.js
+++ JS Advanced/DOM Introduction/1. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumber = document.getElementById('firstNumber').value;
+    const secondNumber = document.getElementById('secondNumber').value;
+
+    const result = Number(firstNumber) - Number(secondNumber);
+
+    document.getElementById('result').textContent = result;
 }
--- JS Advanced/DOM Introduction/3. Accordion/accordion.js
+++ JS Advanced/DOM Introduction/3. Accordion/accordion.js
@@ -1,3 +1,8 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.querySelector('.button');
+    let divExtra = document.querySelector('#extra');
+
+    const isHidden = button.textContent == 'More';
+    divExtra.style.display = (isHidden) ? 'block' : 'none';
+    button.textContent = (isHidden) ? 'Less' : 'More';
 }
