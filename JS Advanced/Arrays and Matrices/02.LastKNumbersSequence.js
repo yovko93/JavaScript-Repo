a6b24c1dc8bc14d6e361022dd026c204614e3899
Yovko Garkov <iovko1993@gmail.com>
2021-09-10 23:50:37 +0300

    Update 02.LastKNumbersSequence.js

--- JS Advanced/Arrays and Matrices/02.LastKNumbersSequence.js
+++ JS Advanced/Arrays and Matrices/02.LastKNumbersSequence.js
@@ -2,16 +2,22 @@ function solve(n, k) {
     n = Number(n);
     k = Number(k);
 
-    let arr = [1, 1];
+    let result = [1];
 
-    for (let i = 2; i < n; i++) {
-        arr[i] = (arr[i - 1] + arr[i - 2]);
+    for (let i = 1; i < n; i++) {
+        let startIndex = k;
 
-
+        if (k > i) {
+            startIndex = i
+        }
+        
+        let currentArr = result.slice(result.length - startIndex);
+        let sum = currentArr.reduce((x, i) => x + i);
+        
+        result[i] = sum;
     }
 
-
-    console.log();
+    return result;
 }
 
 console.log(solve(6, 3));
