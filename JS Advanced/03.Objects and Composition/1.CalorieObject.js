265030349f33f06a540365a4061f7425ecec1dd9
Yovko Garkov <iovko1993@gmail.com>
2021-09-28 11:22:44 +0300

    Update 1.CalorieObject.js

--- JS Advanced/03.Objects and Composition/1.CalorieObject.js
+++ JS Advanced/03.Objects and Composition/1.CalorieObject.js
@@ -8,4 +8,19 @@ function calorieObject(arr){
     return result;
 }
 
+// // second way
+// function calorieObject(arr){
+//     let result = {};
+    
+//     arr.forEach((el, i) => {
+//         if (i % 2 == 0) {
+//             result[el] = undefined;
+//         } else {
+//             result[arr[i-1]] = el;
+//         }
+//     });
+
+//     return result;
+// }
+
 console.log(calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
