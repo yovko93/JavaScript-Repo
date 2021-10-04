1e497925309c83169daaede9f6435689baac4e8d
Yovko Garkov <iovko1993@gmail.com>
2021-10-04 20:21:34 +0300

    Update CollectListItems.js

--- JS Advanced/04.DOM Introduction/02.Collect List Items/CollectListItems.js
+++ JS Advanced/04.DOM Introduction/02.Collect List Items/CollectListItems.js
@@ -4,3 +4,13 @@ function extractText() {
 
     document.getElementById('result').value = elementText.join('\n');
 }
+
+// 2nd way
+// function extractText() {
+//     let listItemsQuery = document.querySelectorAll('#items li');
+//     let result = document.getElementById('result');
+
+//     for (const listItem of listItemsQuery) {
+//         result.textContent += listItem.textContent + '\n';
+//     }
+// }
