3f4e67d08ad8d2af0e17bc8eefca0ea79c31e9eb
Yovko Garkov <iovko1993@gmail.com>
2021-10-04 20:21:37 +0300

    Update solution.js

--- JS Advanced/04.DOM Introduction/2. Pascal or Camel Case/solution.js
+++ JS Advanced/04.DOM Introduction/2. Pascal or Camel Case/solution.js
@@ -1,3 +1,40 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let currentCase = document.getElementById('naming-convention').value;
+
+  let textArr = input.toLowerCase().split(' ');
+  let result = document.getElementById('result');
+
+  switch (currentCase) {
+      case 'Camel Case':
+          let camelCase = '';
+
+          camelCase += textArr[0];
+
+          for (let i = 1; i < textArr.length; i++) {
+              let element = textArr[i];
+              element = element[0].toUpperCase().concat(element.slice(1));
+
+              camelCase += element;
+          }
+
+          result.textContent = camelCase;
+          break;
+      case 'Pascal Case':
+          let pascalCase = '';
+
+          for (let i = 0; i < textArr.length; i++) {
+              let element = textArr[i];
+              element = element[0].toUpperCase().concat(element.slice(1));
+
+              pascalCase += element;
+          }
+          
+          result.textContent = pascalCase;
+          break;
+      default:
+          result.textContent = 'Error!';
+          break;
+  }
+
 }
