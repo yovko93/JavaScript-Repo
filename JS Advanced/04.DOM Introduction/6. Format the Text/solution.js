c9a0982d5e9b97df19bbd1bd7909e15c20a0cedc
Yovko Garkov <iovko1993@gmail.com>
2021-10-06 19:37:38 +0300

    Update solution.js

--- JS Advanced/04.DOM Introduction/6. Format the Text/solution.js
+++ JS Advanced/04.DOM Introduction/6. Format the Text/solution.js
@@ -1,3 +1,31 @@
+// 100/100
 function solve() {
-  //TODO
+  let inputStr = document.getElementById('input').value;
+  let output = document.getElementById('output');
+
+  let input = inputStr.split('.').filter((p) => p.length > 0);
+
+  for (let i = 0; i < input.length; i += 3) {
+      let arr = [];
+      for (let y = 0; y < 3; y++) {
+          if (input[i + y]) {
+              arr.push(input[i + y]);
+          }
+      }
+      let paragraph = arr.join('. ') + '.';
+      output.innerHTML += `<p>${paragraph}</p>`;
+  }
 }
+
+//-------------------------
+// 60/100
+// function solve() {
+//   let inputText = document.getElementById('input').value;
+//   let outputText = document.getElementById('output');
+//   let sentences = inputText.split('.').filter((p) => p.length > 0);
+//   let paragraphs = Math.ceil(sentences.length / 3);
+
+//   for (let i = 0; i < paragraphs; i++) {
+//     outputText.innerHTML += `<p> ${sentences.splice(0, 3).join('. ')}. </p>`;
+//   }
+// }
