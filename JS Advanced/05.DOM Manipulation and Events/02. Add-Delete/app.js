f381c8692cfabf8aeae303504c108af91dc93fcb
Yovko Garkov <iovko1993@gmail.com>
2021-10-07 14:59:39 +0300

    Update app.js

--- JS Advanced/05.DOM Manipulation and Events/02. Add-Delete/app.js
+++ JS Advanced/05.DOM Manipulation and Events/02. Add-Delete/app.js
@@ -21,3 +21,25 @@ function addItem() {
         return element;
     }
 }
+
+// // 2nd way
+// //--------------------
+// function addItem() {
+//     let newText = document.getElementById('newText');
+//     let items = document.getElementById('items');
+
+//     let liElement = document.createElement('li');
+//     liElement.textContent = newText.value;
+
+//     let deleteBtn = document.createElement('a');
+//     deleteBtn.setAttribute('href', '#');
+//     deleteBtn.textContent = '[Delete]';
+//     deleteBtn.addEventListener('click', (e) => {
+//         e.currentTarget.parentNode.remove();
+//     })
+    
+//     liElement.appendChild(deleteBtn);
+//     items.appendChild(liElement);
+
+//     newText.value = '';
+// }
