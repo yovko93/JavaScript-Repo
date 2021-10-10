fcb9d2a4588f9b8e2a8c11e8ce56f77c14eaaf4b
Yovko Garkov <iovko1993@gmail.com>
2021-10-10 21:26:50 +0300

    Update script.js

--- JS Advanced/05.DOM Manipulation and Events/1. Sections/script.js
+++ JS Advanced/05.DOM Manipulation and Events/1. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+   let mainDivElement = document.getElementById('content');
+
+   for (const string of words) {
+      let divElement = document.createElement('div');
+      let paragraph = document.createElement('p');
+
+      paragraph.textContent = string;
+      paragraph.style.display = 'none';
+
+      divElement.appendChild(paragraph);
+      mainDivElement.appendChild(divElement);
+
+      divElement.addEventListener('click', (e) => {
+         e.target.children[0].style.display = 'block';
+      })
+   }
+
+   // mainDivElement.addEventListener('click', (e) => {
+   //    let paragraph = e.target.children[0];
+   //    paragraph.style.display = 'block';
+   // });
 }
