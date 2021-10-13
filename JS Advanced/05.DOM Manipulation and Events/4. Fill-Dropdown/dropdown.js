8ee6d9739716fc28ee232a05c099540f9ab7af0e
Yovko Garkov <iovko1993@gmail.com>
2021-10-13 13:38:28 +0300

    Update dropdown.js

--- JS Advanced/05.DOM Manipulation and Events/4. Fill-Dropdown/dropdown.js
+++ JS Advanced/05.DOM Manipulation and Events/4. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = newItemText.value;
+    optionElement.value = newItemValue.value;
+    document.getElementById('menu').appendChild(optionElement);
+
+    newItemText.value = '';
+    newItemValue.value = '';
 }
