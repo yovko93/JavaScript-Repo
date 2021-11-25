268628b89bd8ae8c7d188d9e41fe54f6efd88e9d
Yovko Garkov <iovko1993@gmail.com>
2021-11-26 00:49:51 +0200

    Create card.js

---
+++ JS Applications/06.Client Side Rendering/01.Contacts/card.js
@@ -0,0 +1,19 @@
+import { html } from './node_modules/lit-html/lit-html.js';
+import { styleMap } from './node_modules/lit-html/directives/style-map.js'
+
+const cardTemplate = (data) => html`
+<div class="contact card">
+    <div>
+        <i class="far fa-user-circle gravatar"></i>
+    </div>
+    <div class="info">
+        <h2>Name: ${data.name}</h2>
+        <button class="detailsBtn">Details</button>
+        <div class="details" id=${data.id} style=${styleMap({display: data.isVisible ? 'block' : 'none'})}">
+            <p>Phone number: ${data.phoneNumber}</p>
+            <p>Email: ${data.email}</p>
+        </div>
+    </div>
+</div>`;
+
+export default cardTemplate;
