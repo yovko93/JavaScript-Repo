7e4b45a595e4bc7ac322d064ef60f468ab5213e0
Yovko Garkov <iovko1993@gmail.com>
2021-11-28 23:30:45 +0200

    Update solution.js

--- JS Applications/06.Client-Side-Rendering/5.Table-Search/solution.js
+++ JS Applications/06.Client-Side-Rendering/5.Table-Search/solution.js
@@ -1,8 +1,17 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-   function onClick() {
-      //   TODO:
+// start: (executed on app load)
+// fetch and parse data
+// call update
 
-   }
-}
+// update: (executed on search)
+// compare student info with search parameter
+// interpolate template with data
+// render result
+
+// template: (table row)
+// populate student info inside row
+// display class based on parameter
+
+// GET http://localhost:3030/jsonstore/advanced/table
+
